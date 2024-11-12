04d3f87372ca299fb80b3fc0beb9a12d19895a96
stiangglanda <leander.kieweg@gmail.com>
2024-11-12 17:38:05 +0100

    feat: Vector Map, 3d surrounding Buildings

--- app/components/map-component.tsx
+++ app/components/map-component.tsx
@@ -8,38 +8,15 @@ export default function MapComponent() {
   useEffect(() => {
     if (!mapContainer.current) return;
 
+    const key = "wYonyRi2hNgJVH2qgs81"; //TODO: just for testing, replace with a custom server if needed
+    const tileUrl = `https://api.maptiler.com/maps/basic-v2/style.json?key=${key}`;
+
+    const MAPTILER_KEY = "get_your_own_OpIi9ZULNHzrESv6T2vL";
+
     // Initialize the MapLibre map
     const map = new maplibregl.Map({
       container: mapContainer.current,
-      style: {
-        version: 8,
-        name: "Raster tiles",
-        center: [0, 0],
-        zoom: 0,
-        sources: {
-          "raster-tiles": {
-            type: "raster",
-            tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-            tileSize: 256,
-            minzoom: 0,
-            maxzoom: 19,
-          },
-        },
-        layers: [
-          {
-            id: "background",
-            type: "background",
-            paint: {
-              "background-color": "#e0dfdf",
-            },
-          },
-          {
-            id: "simple-tiles",
-            type: "raster",
-            source: "raster-tiles",
-          },
-        ],
-      },
+      style: tileUrl, // URL to map style JSON
       center: [-87.616_94, 41.866_25],
       zoom: 15.99,
       pitch: 40,
@@ -48,6 +25,61 @@ export default function MapComponent() {
     });
 
     map.on("load", () => {
+      // Insert the layer beneath any symbol layer.
+      const layers = map.getStyle().layers;
+
+      let labelLayerId;
+      for (const layer of layers) {
+        if (layer.type === "symbol" && layer.layout["text-field"]) {
+          labelLayerId = layer.id;
+          break;
+        }
+      }
+
+      map.addSource("openmaptiles", {
+        url: `https://api.maptiler.com/tiles/v3/tiles.json?key=${MAPTILER_KEY}`,
+        type: "vector",
+      });
+
+      map.addLayer(
+        {
+          id: "3d-buildings",
+          source: "openmaptiles",
+          "source-layer": "building",
+          type: "fill-extrusion",
+          minzoom: 15,
+          filter: ["!=", ["get", "hide_3d"], true],
+          paint: {
+            "fill-extrusion-color": [
+              "interpolate",
+              ["linear"],
+              ["get", "render_height"],
+              0,
+              "lightgray",
+              200,
+              "royalblue",
+              400,
+              "lightblue",
+            ],
+            "fill-extrusion-height": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              16,
+              ["get", "render_height"],
+            ],
+            "fill-extrusion-base": [
+              "case",
+              [">=", ["get", "zoom"], 16],
+              ["get", "render_min_height"],
+              0,
+            ],
+          },
+        },
+        labelLayerId,
+      );
       // Add the GeoJSON source for floorplan
       map.addSource("floorplan", {
         type: "geojson",
