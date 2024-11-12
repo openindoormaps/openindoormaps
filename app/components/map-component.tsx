df3e1411b3785e7763e52adcaeffd1fc045d6538
stiangglanda <leander.kieweg@gmail.com>
2024-11-12 17:07:08 +0100

    feat: 3d geojson rendering

--- app/components/map-component.tsx
+++ app/components/map-component.tsx
@@ -4,26 +4,74 @@ import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function MapComponent() {
   const mapContainer = useRef<HTMLDivElement>(null);
-  const key = "wYonyRi2hNgJVH2qgs81"; //TODO: just for testing, replace with a custom server if needed
-  const tileUrl = `https://api.maptiler.com/maps/basic-v2/style.json?key=${key}`;
 
   useEffect(() => {
     if (!mapContainer.current) return;
 
-    // Initialize MapLibre map
+    // Initialize the MapLibre map
     const map = new maplibregl.Map({
       container: mapContainer.current,
-      style: tileUrl, // URL to map style JSON
-      center: [0, 0], // Initial center [longitude, latitude]
-      zoom: 0,
+      style: {
+        version: 8,
+        name: "Raster tiles",
+        center: [0, 0],
+        zoom: 0,
+        sources: {
+          "raster-tiles": {
+            type: "raster",
+            tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+            tileSize: 256,
+            minzoom: 0,
+            maxzoom: 19,
+          },
+        },
+        layers: [
+          {
+            id: "background",
+            type: "background",
+            paint: {
+              "background-color": "#e0dfdf",
+            },
+          },
+          {
+            id: "simple-tiles",
+            type: "raster",
+            source: "raster-tiles",
+          },
+        ],
+      },
+      center: [-87.616_94, 41.866_25],
+      zoom: 15.99,
+      pitch: 40,
+      bearing: 20,
+      antialias: true,
     });
 
-    map.addControl(new maplibregl.NavigationControl(), "top-right");
+    map.on("load", () => {
+      // Add the GeoJSON source for floorplan
+      map.addSource("floorplan", {
+        type: "geojson",
+        data: "https://maplibre.org/maplibre-gl-js/docs/assets/indoor-3d-map.geojson",
+      });
+
+      // Add a layer for room extrusion
+      map.addLayer({
+        id: "room-extrusion",
+        type: "fill-extrusion",
+        source: "floorplan",
+        paint: {
+          "fill-extrusion-color": ["get", "color"],
+          "fill-extrusion-height": ["get", "height"],
+          "fill-extrusion-base": ["get", "base_height"],
+          "fill-extrusion-opacity": 0.5,
+        },
+      });
+    });
 
     return () => {
       map.remove();
     };
-  }, [tileUrl]);
+  }, []);
 
   return (
     <div
