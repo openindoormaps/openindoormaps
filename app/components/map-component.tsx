1356d7edd99275b3277cc1cdbc46e81753d806b7
Mark <113889912+kno20205@users.noreply.github.com>
2024-11-17 19:47:16 +0100

    feat: integrate Maplibre Geocoder for forward geocoding in map component

--- app/components/map-component.tsx
+++ app/components/map-component.tsx
@@ -1,7 +1,11 @@
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect, useRef } from "react";
-
+import MaplibreGeocoder, {
+  MaplibreGeocoderApi,
+  CarmenGeojsonFeature,
+  MaplibreGeocoderFeatureResults,
+} from "@maplibre/maplibre-gl-geocoder";
 export default function MapComponent() {
   const mapContainer = useRef<HTMLDivElement>(null);
   const key = "wYonyRi2hNgJVH2qgs81"; //TODO: just for testing, replace with a custom server if needed
@@ -29,16 +33,61 @@ export default function MapComponent() {
       "bottom-right",
     );
 
+    const geocoderApi: MaplibreGeocoderApi = {
+      // required
+      forwardGeocode: async (config) => {
+        const features: CarmenGeojsonFeature[] = [];
+        try {
+          const request = `https://nominatim.openstreetmap.org/search?q=${
+            config.query
+          }&format=geojson&polygon_geojson=1&addressdetails=1`;
+          const response = await fetch(request);
+          const geojson = await response.json();
+          for (const feature of geojson.features) {
+            const center = [
+              feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
+              feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
+            ];
+            const point: CarmenGeojsonFeature = {
+              id: feature.id,
+              type: "Feature",
+              geometry: {
+                type: "Point",
+                coordinates: center,
+              },
+              place_name: feature.properties.display_name,
+              properties: feature.properties,
+              text: feature.properties.display_name,
+              place_type: ["place"],
+            };
+            features.push(point);
+          }
+        } catch (error) {
+          console.error(`Failed to forwardGeocode with error: ${error}`);
+        }
+
+        return {
+          features,
+        } as MaplibreGeocoderFeatureResults;
+      },
+
+      // optional
+      reverseGeocode: async (config) => {
+        config;
+        return {} as MaplibreGeocoderFeatureResults;
+      },
+    };
+
+    // Pass in or define a geocoding API that matches the above
+    const geocoder = new MaplibreGeocoder(geocoderApi, {
+      maplibregl: maplibregl,
+    });
+    map.addControl(geocoder);
+
     return () => {
       map.remove();
     };
   }, [tileUrl]);
 
-  return (
-    <div
-      ref={mapContainer}
-      className="size-full"
-      style={{ width: "100%", height: "100%" }}
-    ></div>
-  );
+  return <div ref={mapContainer} className="size-full"></div>;
 }
