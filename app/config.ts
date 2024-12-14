25697e07f020412c123c2ee07d596533c5b4d20a
Mark <113889912+kno20205@users.noreply.github.com>
2024-12-15 00:42:52 +0100

    refactor: remove unnecessary map options from configuration

--- app/config.ts
+++ app/config.ts
@@ -6,9 +6,6 @@ const config = {
       "https://api.maptiler.com/maps/basic-v2/style.json?key=wYonyRi2hNgJVH2qgs81",
     center: [-87.616_94, 41.866_25],
     zoom: 15.99,
-    pitch: 40,
-    bearing: 20,
-    antialias: true,
   } as maplibregl.MapOptions,
 };
 export default config;
