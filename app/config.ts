21a9daf73d94acf1784c92bc4a9f37573bd99743
54toshi <julianeterribla@protonmail.com>
2025-01-25 00:24:01 +0100

    feat: specify maptiler API key in .env

--- app/config.ts
+++ app/config.ts
@@ -2,8 +2,7 @@ const config = {
   geoCodingApi: "https://nominatim.openstreetmap.org",
   routingApi: "https://router.project-osrm.org/route/v1",
   mapConfig: {
-    style:
-      "https://api.maptiler.com/maps/basic-v2/style.json?key=wYonyRi2hNgJVH2qgs81",
+    style: `https://api.maptiler.com/maps/basic-v2/style.json?key=${process.env.MAPTILER_KEY}`,
     center: [3.110_97, 45.758_887],
     zoom: 17,
   } as maplibregl.MapOptions,
