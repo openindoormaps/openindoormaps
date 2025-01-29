d90aa04a1bb2efd4e4dc45b2bfa841eb9ed2dde1
stiangglanda <leander.kieweg@gmail.com>
2025-01-29 13:03:33 +0100

    refactor: update MapComponent to use consistent map instance handling and improve config values

--- app/config.ts
+++ app/config.ts
@@ -4,7 +4,7 @@ const config = {
   mapConfig: {
     style:
       "https://api.maptiler.com/maps/basic-v2/style.json?key=wYonyRi2hNgJVH2qgs81",
-    center: [3.110_827_521_806_985, 45.759_220_930_846_475],
+    center: [3.110_97, 45.758_887],
     zoom: 17,
   } as maplibregl.MapOptions,
 };
