9036b72eab6d9c93292db0959e562bf627f870ef
Mark <knotzermark@gmail.com>
2025-02-06 17:37:21 +0100

    chore: adapt map configuration for demo release

--- app/config.ts
+++ app/config.ts
@@ -4,7 +4,13 @@ const config = {
   mapConfig: {
     style: "https://tiles.openfreemap.org/styles/bright",
     center: [3.110_97, 45.758_887],
-    zoom: 17,
+    zoom: 19,
+    bearing: 60,
+    pitch: 40,
+    maxBounds: [
+      [3.098_579_765_873_666, 45.753_206_988_746_97],
+      [3.120_672_060_142_396_7, 45.764_883_726_343_584],
+    ],
   } as maplibregl.MapOptions,
 };
 export default config;
